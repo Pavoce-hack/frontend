c8d3e05cfd2c05e1bf43c419177ea05ad31cc5b4
Olumide Micheal <olumidemicheal7@gmail.com>
2023-08-11 09:20:35 +0100

    handled authentication

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,35 +3,44 @@
 import CustomButton from "@/components/btn";
 import Navbar from "@/pages/homepage/common/nav";
 import Link from "next/link";
+import { useAccount } from 'wagmi';
+import React from "react";
 
-const Auth = () => {
+const Auth: React.FC = () => {
+    const { address } = useAccount();
+
+
     return (
         <div className="center min-h-screen w-screen pt-10">
             <div className='w-11/12 flex items-center flex-col'>
                 <Navbar />
-                <div className="flex items-center flex-col gap-6 mt-16 min-h-[80vh] w-full">
-                    <h2 className="text-xl font-bold text-center">
-                        Just a moment...
-                    </h2>
-                    <p className="text-center text-md">
-                        Let us know you
-                    </p>
-                    <div className="flex flex-col gap-2 w-1/3">
-                        <label htmlFor="name" className="font-semibold">Business Name:</label>
-                        <input type="text" name="name" id="businessName" placeholder="Enter your Business Name" className='border border-[#B8B8B8] outline-none border-DarkGray px-5 py-3 text-black rounded-lg' />
+                {address ? (
+                    <div className="flex items-center flex-col gap-6 mt-16 min-h-[80vh] w-full">
+                        <h2 className="text-xl font-bold text-center">
+                            Just a moment...
+                        </h2>
+                        <p className="text-center text-md">
+                            Let us know you
+                        </p>
+                        <div className="flex flex-col gap-2 w-1/3">
+                            <label htmlFor="name" className="font-semibold">Business Name:</label>
+                            <input type="text" name="name" id="businessName" placeholder="Enter your Business Name" className='border border-[#B8B8B8] outline-none border-DarkGray px-5 py-3 text-black rounded-lg' />
+                        </div>
+                        <div className="flex flex-col gap-2 w-1/3">
+                            <label htmlFor="photo" className="font-semibold">Upload Photo</label>
+                            <input type="file" name="photo" id="businessPhoto" className='border border-[#B8B8B8] outline-none border-DarkGray px-5 py-3 text-black rounded-lg' />
+                        </div>
+                        <div className="end w-1/3">
+                            <Link href="/profile">
+                                <CustomButton onClick={() => {}} padding="10px 30px" background="#3A62F2" textColor="#FFFFFF">
+                                    Continue
+                                </CustomButton>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-2 w-1/3">
-                        <label htmlFor="photo" className="font-semibold">Upload Photo</label>
-                        <input type="file" name="photo" id="businessPhoto" className='border border-[#B8B8B8] outline-none border-DarkGray px-5 py-3 text-black rounded-lg' />
-                    </div>
-                    <div className="end w-1/3">
-                        <Link href="/profile">
-                            <CustomButton onClick={() => {}} padding="10px 30px" background="#3A62F2" textColor="#FFFFFF">
-                                Continue
-                            </CustomButton>
-                        </Link>
-                    </div>
-                </div>
+                ) : (
+                    <div className="min-h-[87vh] w-full center font-semibold text-md">Connect your wallet to get started</div>
+                )}
             </div>
         </div>
     );
--- src/app/profile/common/dashboard.tsx
+++ src/app/profile/common/dashboard.tsx
@@ -5,6 +5,11 @@ import Logo from "@/pages/homepage/common/logo";
 import ProfileNav from "./navbar";
 import { DashboardUtils } from '@/utils/dashboard';
 import Image from 'next/image';
+import Transaction from './transcaction';
+import Client from './client';
+import Services from './services';
+import Profile from './profile';
+import Invoice from './invoice';
 
 const Dashboard: React.FC = () => {
     const [activeTab, setActiveTab] = useState(0);
@@ -16,13 +21,15 @@ const Dashboard: React.FC = () => {
     const renderTabContent = () => {
         switch (activeTab) {
           case 0:
-            return <Account />;
+            return <Profile />;
           case 1:
-            return <MyProduct />;
+            return <Invoice />;
           case 2:
-            return <Add />;
+            return <Services />;
           case 3:
-            return <Track />;
+            return <Client />;
+          case 4:
+            return <Transaction />;
           default:
             return null;
         }
@@ -31,10 +38,10 @@ const Dashboard: React.FC = () => {
     return (
         <div className="w-full h-screen center">
             <div className="w-[20%] pt-10 bg-lightBlue h-full flex items-center flex-col">
-                <div className="w-1/2">
+                <div className="w-[80%]">
                     <Logo />
                 </div>
-                <div className='flex mt-10 w-[80%] flex-col'>
+                <div className='flex mt-16 w-[80%] flex-col'>
                     {DashboardUtils.map((tab, index) => (
                         <div
                             key={index}
@@ -51,6 +58,7 @@ const Dashboard: React.FC = () => {
             </div>
             <div className="w-[80%] h-full pr-16">
                 <ProfileNav />
+                {renderTabContent()}
             </div>
         </div>
     );
