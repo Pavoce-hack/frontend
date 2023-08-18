4118e9b9b23387c9d9f70da0bc0a5206c27419c6
Olumide Micheal <olumidemicheal7@gmail.com>
2023-08-18 21:27:56 +0100

    test

--- src/app/profile/common/date-time.ts
+++ src/app/profile/common/date-time.ts
@@ -1,3 +1,5 @@
+'use client'
+
 function getCurrentDateTime() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
